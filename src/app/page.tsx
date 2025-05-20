5f12fd5a041309dfdb2a8d446bc24db2ac0f9cd9
yeobi01 <yeobi_01@naver.com>
2025-05-20 15:19:36 +0900

    chore: 수정해보기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,16 +13,8 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
+          <li className="mb-2 tracking-[-.01em]">k8s study</li>
+          <li className="tracking-[-.01em]">끝났을지도..</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
